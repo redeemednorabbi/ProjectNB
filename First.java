61ab45435880955ea33aea36fb5dc3f63d2ad326
RedeemedNoRabbi <redeemednotquitearabbi@gmail.com>
2025-01-20 21:35:50 -0500

    Our first commit, assignement 1

---
+++ First.java
@@ -0,0 +1,8 @@
+// Nathan Brooks 1/20/25, first project.
+// What I want to be is someone who writes software algorithms that trade in stock markets.
+public class First {
+    public static void main(String args[])
+    {
+        System.out.println("\nHola mundo\nTitle: Software Developer\nRationale: Self sufficient, profitable\nIncome: $130,160");
+    }
+}
